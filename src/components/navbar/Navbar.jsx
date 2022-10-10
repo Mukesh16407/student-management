6336dcf2e603d13bd80430aeb0c52ca6aafe71c0
Mukesh16407 <mkvermashk@gmail.com>
2022-10-10 10:59:50 +0530

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { Box, AppBar, Toolbar, IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./navbar.css";
 
 export const Navbar = () => {
+
+    const [showProfile, setProfile] = useState(false);
+    const [showSidebar, setSidebar] = useState("none");
+
+    const { pathname } = useLocation();
+    
+    const showSideBar = () => {
+        if (showSidebar === "none") {
+            setSidebar("visible")
+        }
+        else if (showSidebar === "visible") {
+            setSidebar("none")
+        }
+    }
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+             {pathname === "/" ||
+                                    pathname === "/admin" ||
+                                    pathname === "/signup" ?
+                                    null :
+                                    <MenuIcon
+                                        onClick={showSideBar}
+                                    />}
+            </IconButton>
+           
+          </Toolbar>
+         
+        </AppBar>
+      </Box>
+    </>
+  );
+};
